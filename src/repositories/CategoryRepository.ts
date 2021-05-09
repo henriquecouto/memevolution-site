895a722da1969323832fa19b12608a2848569352
Henrique Couto <email@henriquecouto.com.br>
2021-05-09 19:59:01 -0300

    adds a getter by id to category repository

--- src/repositories/CategoryRepository.ts
+++ src/repositories/CategoryRepository.ts
@@ -1,5 +1,7 @@
 import Category from "../entities/Category";
 
 export default interface ICategoryRepository {
-  getAllCategories: () => Promise<Array<Category> | undefined>;
+  categories: Array<Category>;
+  loadAllCategories: () => Promise<void>;
+  getCategoryById: (id: string) => Category;
 }
