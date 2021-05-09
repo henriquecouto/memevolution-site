3d34897f90500b5807626b8f6302c2381d42ccfc
Henrique Couto <email@henriquecouto.com.br>
2021-05-08 22:46:26 -0300

    creates category repository

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import App from "./App";
 import { ThemeProvider } from "styled-components";
 import standard from "./styles/themes/standard";
 import { StrictMode } from "react";
+import FirebaseCategoryRepository from "./repositories/implementations/FirebaseCategoryRepository";
+
+const firebaseCategoryRepository = new FirebaseCategoryRepository();
+
+(async () =>
+  console.log(await firebaseCategoryRepository.getAllCategories()))();
 
 ReactDOM.render(
   <StrictMode>
